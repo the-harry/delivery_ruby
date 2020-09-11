cda4f91c0ecbff7e75633e34ed43e37f16e9ef79
Matheus Acosta <matheusthebr@gmail.com>
2020-09-11 11:11:50 -0300

    Created basic order controller logic.

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,8 +2,55 @@
 
 class Api::V1::OrdersController < ApplicationController
   def create
-    render status: :unprocessable_entity if params.blank?
+    builder = PayloadBuilder.new(order_params)
+    order = Order.new(builder.payload)
 
-    render status: :created
+    if order.save
+      render status: :created if ProcessOrderService.call(builder.build!)
+
+      render status: :service_unavailable
+    else
+      render status: :unprocessable_entity, json: { errors: order.errors.full_messages }
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :external_code,
+      :store_id,
+      :sub_total,
+      :delivery_fee,
+      :total,
+      :country,
+      :state,
+      :city,
+      :district,
+      :street,
+      :complement,
+      :latitude,
+      :longitude,
+      :dt_order_create,
+      :postal_code,
+      :number,
+      customer_attributtes: %i[
+        external_code
+        name
+        email
+        contact
+      ],
+      items_attributtes: %i[
+        external_code
+        name
+        price
+        quantity
+        total
+      ],
+      payments_attributtes: %i[
+        type
+        value
+      ]
+    )
   end
 end
