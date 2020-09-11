f3af12949d7890728a436b6ff9c6d2a58df0008d
Matheus Acosta <matheusthebr@gmail.com>
2020-09-11 09:50:10 -0300

    Created failure scenario for payload parser.

--- spec/models/payload_builder_spec.rb
+++ spec/models/payload_builder_spec.rb
@@ -4,13 +4,24 @@ require 'rails_helper'
 
 describe PayloadBuilder do
   describe '.build!' do
-    let(:input_json) { File.read('spec/fixtures/raw_order.json') }
-    let(:expected_json) { File.read('spec/fixtures/processed_order.json') }
+    let(:good_json) { File.read('spec/fixtures/raw_order.json') }
+    let(:bad_json) { File.read('spec/fixtures/raw_order_missing_fields.json') }
 
-    it 'returns expect json' do
-      built = described_class.new(input_json).build!
+    context 'when it succeeds' do
+      let(:expected_json) { File.read('spec/fixtures/processed_order.json') }
+      let(:built_json) { described_class.new(good_json).build! }
 
-      expect(JSON.parse(built)).to eq(JSON.parse(expected_json))
+      it 'returns expect json' do
+        expect(JSON.parse(built_json)).to eq(JSON.parse(expected_json))
+      end
+    end
+
+    context 'when it fails' do
+      let(:built_json) { described_class.new(bad_json).build! }
+
+      it 'fill in missing keys with null' do
+        expect(built_json).to match(/"complement":null/)
+      end
     end
   end
 end
