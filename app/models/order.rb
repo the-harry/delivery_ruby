8f493660462e963253866f29f5ce24a0f238c1c3
Matheus Acosta <matheusthebr@gmail.com>
2020-09-11 11:11:05 -0300

    Accepts nested attrs for order and validate then.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,15 @@ class Order < ApplicationRecord
   has_many :items
   has_many :payments
 
+  accepts_nested_attributes_for :customer
+  validates_associated :customer
+
+  accepts_nested_attributes_for :items
+  validates_associated :items
+
+  accepts_nested_attributes_for :payments
+  validates_associated :payments
+
   validates :external_code,
             :store_id,
             :sub_total,
